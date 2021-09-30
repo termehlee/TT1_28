7b43a33f0eb2976e24a39fabef0cecad4517988f
cwf97 <chua.wen.feng97@gmail.com>
2021-09-30 13:27:00 +0800

    Remove Login and Register from Nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,9 +4,6 @@ import { BrowserRouter, Link } from "react-router-dom";
 const Navbar = () => {
     const [navs] = useState([
         {link: '/', value: 'Home', id:1},
-        {link: '/cart', value: 'Shopping Cart', id:3},
-        {link: '/register', value: 'Register', id:2},
-        {link: '/login', value: 'Login', id:3},
         {link: '/cart', value: 'Cart', id:3},
     ])
     return ( 
