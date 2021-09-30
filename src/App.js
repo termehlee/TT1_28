cbb9d008a9a8213e46181d7afb04247288c46414
zentan2018 <54927578+zentan2018@users.noreply.github.com>
2021-09-30 15:34:37 +0800

    Resolve Comflict

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
       <h1>Application</h1>
       <BrowserRouter>
         <Switch>
-          {/* <Route exact path={"/products"} component={ProductList} /> */}
+          <Route exact path={"/products"} component={ProductList} />
           <Route path="/dashboard">
             <Dashboard />
           </Route>
