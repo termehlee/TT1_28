f9ba337311fa9c1fbd0a794164a8327bdb0d59cf
termehlee <e0324346@u.nus.edu>
2021-09-30 11:21:53 +0800

    Firebase setup

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
 
-app.use(cors())
+// app.use(cors())
 
-app.use('/login', (req, res) => {
-  res.send({
-    token: 'test123'
-  });
-});
+// app.use('/login', (req, res) => {
+//   res.send({
+//     token: 'test123'
+//   });
+// });
 
-app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
+// app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
 
-console.log("test")
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAklBEHqjVStt-KfpRmtBD_oHgSMb_Y9K0",
+  authDomain: "techtreck-8a422.firebaseapp.com",
+  projectId: "techtreck-8a422",
+  storageBucket: "techtreck-8a422.appspot.com",
+  messagingSenderId: "634362721671",
+  appId: "1:634362721671:web:f59e7fb283d4d0f9de4f22",
+  measurementId: "G-RJXBZBGXLN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
