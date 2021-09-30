8d72ec367011bb6068c54aa1e635eb4d9267a1c0
kek min yi <mkek002@e.ntu.edu.sg>
2021-09-30 11:45:25 +0800

    added OrderService and pulled from firebase branch

---
+++ src/services/OrderService.js
@@ -0,0 +1,33 @@
+import firebase from "../firebase";
+
+const db = firebase.ref("/products");
+
+const getAllOrders = () => {
+  return db;
+};
+
+const createOrder = (data) => {
+  return db.push(data);
+};
+
+const updateOrder = (key, data) => {
+  return db.child(key).update(data);
+};
+
+const removeOrder = (key) => {
+  return db.child(key).remove();
+};
+
+const removeAllOrders = () => {
+  return db.remove();
+};
+
+const ProductService = {
+  getAllOrders,
+  createOrder,
+  updateOrder,
+  removeOrder,
+  removeAllOrders,
+};
+
+export default ProductService;
